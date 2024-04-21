35b51dd6f10329ceb595a43cce080aafd5eda9e8
Adyan Chowdhury <chowdhuryadyan13@gmail.com>
2024-04-21 01:55:16 -0400

    introdued gemini using test prompt

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,26 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import google.generativeai as genai
 
 app = Flask(__name__)
 cors = CORS(app, origins='*')
+
+with open("api_key.txt", "r") as file:
+  KEY = file.readline()
+
+genai.configure(
+    api_key=KEY
+)
+
+model= genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+response = chat.send_message("Tell me about the moon. Do not use any fancy styling like bolding or anything. Only use text. Make it under 2 sentences.")
 
 @app.route("/gemini-message", methods=['GET'])
 def users():
     return jsonify(
         {
-            "message": [
-                'This is a message from Gemini!',
-            ]
+            "message": response.text
         }
     )
 
