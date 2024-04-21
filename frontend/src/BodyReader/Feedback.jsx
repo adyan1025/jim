e32c5caa193e48d975abcfde9a003c0f0b36cf99
Adyan Chowdhury <chowdhuryadyan13@gmail.com>
2024-04-21 00:15:16 -0400

    working on ai response feature

--- frontend/src/BodyReader/Feedback.jsx
+++ frontend/src/BodyReader/Feedback.jsx
@@ -5,33 +5,35 @@ import axios from 'axios';
 
 
 function Feedback(props) {
-    const [array, setArray] = useState([]);
-    const fetchAPI = async () => {
-        const response = await axios.get("http://127.0.0.1:8080/gemini-message");
-        setArray(response.data.message);
-    }
-    useEffect(() => {
-    fetchAPI()
-    }, []);
-
     const [messages, setMessages] = useState(props.message ? [props.message] : []);
+    const [geminiMessages, setGeminiMessages] = useState([]);
+
     React.useEffect(() => {
         const newMessage = props.message;
+        const fetchAPI = () => {
+            axios.get("http://127.0.0.1:8080/gemini-message")
+                .then(response => {
+                    setGeminiMessages(prevGeminiMessages => [...prevGeminiMessages, response.data.message]);
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
         if (newMessage) {
           setMessages((prevMessages) => [...prevMessages, newMessage]);
+          fetchAPI();
         }
-      }, [props.message]);
+    }, [props.message]);
+
+    
     
     return (
         <div className={style['feedback']}>
             {messages.map((message, index) => (
                 <Message message={message} key={index} />
             ))}
-            {array.map((message, index) => (
-                <div key={index}>
-                    <span>{message}</span>
-                    <br></br>
-                </div>
+            {geminiMessages.map((message, index) => (
+                <Message message={message} key={index} />
             ))}
         </div>
     );
